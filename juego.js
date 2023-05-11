ee428483de1473a1c55529dde66e59ca9d9e1b8e
Juan Solorzano <juanalcachofas6425@gmail.com>
2023-05-11 02:52:34 -0500

    Ingreso número

--- juego.js
+++ juego.js
@@ -1,38 +1,34 @@
 /* GENERAR NUMERO ALEATORIO */
-function aleatorio() {
+
+
+
+function limpiarInput() {
+    document.getElementById("mensaje").innerHTML = "";;
+    document.getElementById("resultado").innerHTML = "";
+    document.getElementById("numero-ingresado").value = "";
     
-    let nu = parseInt(document.getElementById("numUsuario").value);
+  }
 
-    if (!isNaN(nu) && nu<=10) {
-        let ng = Math.floor(Math.random()*11);
-        document.getElementById("numGenerado").innerHTML = ng;
-        if (nu == ng) {
-            document.getElementById("error").style.display = 'none';
-            document.getElementById("respuesta").style.display = 'block';
-            document.getElementById("respuesta").innerHTML = "ADIVINASTE";
-            document.getElementById("numUsuario").value = '';
-            
-        } else {
-            document.getElementById("error").style.display = 'none';
-            document.getElementById("respuesta").style.display = 'block';      
-            document.getElementById("respuesta").innerHTML = "FALLASTE, VUELVE A INTENTARLO";
-            document.getElementById("numUsuario").value = '';
-        }       
-    } else if(!isNaN(nu) && nu<=11){
-        document.getElementById("respuesta").style.display = 'none';
-        document.getElementById("error").style.display = 'block';  
-        document.getElementById("error").innerHTML = "Solo puede ingresar numeros enteros del 0 al 10";
-    }else{
-        document.getElementById("respuesta").style.display = 'none';
-        document.getElementById("error").style.display = 'block';  
-        document.getElementById("error").innerHTML = "No se permiten espacios vacios, letras o caracteres especiales";
-    }
+function generarNumeroAleatorio() {
+    var minimo = 1;
+    var maximo = 10;
+    var numeroAleatorio = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
+    var resultado = document.getElementById("resultado");
+    resultado.innerHTML =numeroAleatorio;
 
-}
-/* FIN GENERAR NUMERO */
-function Reiciniar()
-{
-    document.getElementById("repuesta").style.display='none';
-    document.getElementById("error").style.display='none';
-    document.getElementById("numUsuario").value='';
-}
+    var numeroIngresado = parseInt(document.getElementById("numero-ingresado").value);
+    var mensaje = document.getElementById("mensaje");
+
+    
+    if (numeroIngresado === numeroAleatorio) {
+        mensaje.innerHTML = "¡Ganaste!";
+      } else {
+        mensaje.innerHTML = "Perdiste!! ";
+      }
+      
+  }
+ 
+  
+  
+
+  
